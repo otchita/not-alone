aeb228f4eaae63a62f76f789b7bd7039e35b1172
oussama.tchita <otchita@adscientiam.com>
2020-11-11 18:47:01 +0100

    Create players

---
+++ nalone/players.py
@@ -0,0 +1,13 @@
+"""Players module."""
+
+
+class Player:
+    """A generic player class."""
+
+
+class Creature(Player):
+    """The creature's class object."""
+
+
+class Hunted(Player):
+    """A hunted 's class object."""
