5099a0621c45e9af6f097b56752f76b1b5f310aa
oussama.tchita <otchita@adscientiam.com>
2020-11-11 18:53:19 +0100

    Update requirements in setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,7 @@ from setuptools import setup, find_packages
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
-requirements = [
-    'numpy'
-]
+requirements = []
 
 setup_requirements = ['pytest-runner', ]
 
