ae4925928d36424e84bd65849fa5a7e2c6a1a052
Oussama Tchita <otchita@adscientiam.com>
2020-11-12 00:03:56 +0100

    Update docs

--- nalone/board.py
+++ nalone/board.py
@@ -199,7 +199,7 @@ class BoardType(Enum):
     """Enumerator of board type.
 
     - ``STACKED`` refers to the board where the Artemia spots are stacked
-    at the end of the rescue alley.
+    atthe end of the rescue alley.
 
     - ``ALTERNATING`` refers to the board where the Artemia spots are
     alternating with normal spots.
